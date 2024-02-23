f8399129842c66130614565f439bc6962596bd97
Danilo Augusto <danilo.augmonteiro@gmail.com>
2024-02-22 23:36:25 -0300

    Aula: Criando tabela de candidato

--- src/main/java/com/github/daniloamsilva/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/github/daniloamsilva/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,24 @@
 package com.github.daniloamsilva.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
@@ -20,4 +29,6 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
